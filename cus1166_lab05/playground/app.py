d09b7810f9955f55af016f127470744678ab8ce2
navdeep <navdeep.kaur19@stjohns.edu>
2021-10-02 21:14:34 -0400

    lab05 code

--- cus1166_lab05/playground/app.py
+++ cus1166_lab05/playground/app.py
@@ -1,1 +1,54 @@
+import sys
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+from models import *
 
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+
+@app.route("/")
+def index():
+    courses = Course.query.all()
+    return render_template('index.html', courses=courses)
+
+
+@app.route("/add_course", methods = ["POST"])
+def add_course():
+    #id = request.form.get("id")
+    course_number = request.form.get("course_number")
+    course_title = request.form.get("course_title")
+
+    course = Course(course_number = course_number, course_title = course_title)
+    db.session.add(course)
+    db.session.commit()
+
+    courses = Course.query.all()
+    return render_template('index.html', courses = courses)
+
+@app.route("/register_student/<int:course_id>", methods=["GET", "POST"])
+def register_student(course_id):
+    course = Course.query.get(course_id)
+
+    if request.method == 'Post' :
+        name = request.form.get("name")
+        grade = request.form.get("grade")
+
+    registeredstudents = course.registeredstudents
+    return render_template('course_details.html', course = course, registeredstudents = registeredstudents)
+
+
+def main():
+    if (len(sys.argv)==2):
+        print(sys.argv)
+        if sys.argv[1] == 'createdb':
+             db.create_all()
+        else:
+            print("Run app using 'flask run'")
+            print("To create a database use 'python app.py createdb")
+
+# Run the main method in the context of our flass application# This allows db know about our models.
+if __name__ == "__main__":
+    with app.app_context():
+        main()
--- cus1166_lab05/playground/models.py
+++ cus1166_lab05/playground/models.py
@@ -1,1 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
 
+class Course(db.Model):
+    __tablename__= "courses"
+
+    id = db.Column(db.Integer,primary_key = True)
+    course_number = db.Column(db.String, nullable = False)
+    course_title = db.Column(db.String, nullable = False)
+
+    #Relationship fields
+    registeredStudents = db.relationship("RegisteredStudent", backref="courses", lazy=True)
+
+    def add_registeredstudent(self, name, grade):
+        new_registeredstudent = RegisteredStudent(name=name, grade=grade, course_id=self.id)
+        db.session.add(new_registeredstudent)
+        db.session.commit()
+
+
+class RegisteredStudent(db.Model):
+    __tablename__= "registeredstudent"
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String, nullable = False)
+    grade = db.Column(db.String, nullable = False)
+
+    course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
